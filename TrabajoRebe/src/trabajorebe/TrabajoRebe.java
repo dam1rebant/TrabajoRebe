61728c90bf6beb9ca485a4305a50f1cad5cd67dc
Rebee <Rebee@Rebe>
2016-04-06 13:04:43 +0200

    Prueba Git

---
+++ TrabajoRebe/src/trabajorebe/TrabajoRebe.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajorebe;
+
+/**
+ *
+ * @author Rebee
+ */
+public class TrabajoRebe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=5;
+        String b = "edu";
+        String c = "rebe";
+        
+        System.out.println(c+" y "+b+"van a sacar un "+a);
+    }
+    
+}
